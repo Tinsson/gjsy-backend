1d397804f8a7b9a82a3cf1dfbfd2534b1615363b
Tinsson <695724046@qq.com>
2018-02-13 17:31:30 +0800

    move

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
 
     proxyTable: {
       '/': {
-               target: 'https://wxapitest.xrjinrong.com',
+               target: '',
                // target: 'http://localhost:8081',
                changeOrigin: true,
            }
